ed3c60eace9f26af2b52b0c3178c0102558463c5
Raj Zachariah <rajankz@umd.edu>
2014-10-04 02:59:36 -0400

    simple drawing, toasts and clear

--- src/com/rajankz/personalmessage/DrawingView.java
+++ src/com/rajankz/personalmessage/DrawingView.java
@@ -1,6 +1,14 @@
 package com.rajankz.personalmessage;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Paint.Style;
+import android.graphics.Path;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
@@ -10,14 +18,94 @@ import android.view.View.OnTouchListener;
 
 public class DrawingView extends SurfaceView implements OnTouchListener{
 
+	int red, green, blue, alpha;
+	
+	float prevX, prevY, x, y;
+	Paint mPaint;
+	Path mPath;
+	Color mColor;
+	List<Path> pathList;
+	List<Paint> paintList;
+	
 	public DrawingView(Context context, AttributeSet attrs) {
 		super(context, attrs);
+		setWillNotDraw(false);
+		red = green = blue = alpha = 0;
+		mColor = new Color();
+		pathList = new ArrayList<Path>();
+		paintList = new ArrayList<Paint>();
+	}
+	
+	@Override
+	public void draw(Canvas canvas){
+		super.draw(canvas);
+		int size = pathList.size();
+		//repaint all previous drawings
+		for(int i=0;i<size;i++){
+			canvas.drawPath(pathList.get(i), paintList.get(i));
+		}
+		if(mPath != null && mPaint != null){
+			canvas.drawPath(mPath, mPaint);
+		}
 	}
 	
 	@Override
 	public boolean onTouch(View v, MotionEvent event) {
-		Log.i("hello", "on touch");
-		return false;
+		int action = event.getActionMasked();
+		x = event.getX();
+		y = event.getY();
+		switch(action){
+		case MotionEvent.ACTION_DOWN:
+			mPath = new Path();
+			mPaint = new Paint();
+			mPaint.setStyle(Style.STROKE);
+			mPaint.setAntiAlias(true);
+			mPaint.setStrokeWidth(5);
+			mPaint.setColor(mColor.argb(alpha, red, green, blue));
+
+			prevX = x;
+			prevY = y;
+			mPath.moveTo(x, y);
+			break;
+		case MotionEvent.ACTION_MOVE:
+			mPath.quadTo(prevX, prevY, x, y);
+			mPath.lineTo(x, y);
+			prevX = x;
+			prevY = y;
+			break;
+		case MotionEvent.ACTION_UP:
+			mPath.setLastPoint(x, y);
+			mPath.lineTo(x, y);
+			pathList.add(mPath);
+			paintList.add(mPaint);
+			mPath = null;
+			mPaint = null;
+			break;
+		}
+		invalidate();
+		return true;
+	}
+	
+	public void clearCanvas(){
+		pathList.clear();
+		paintList.clear();
+		invalidate();
+	}
+	
+	public void setRed(int value){
+		red = value;
+	}
+	
+	public void setGreen(int value){
+		green = value;
+	}
+	
+	public void setBlue(int value){
+		blue = value;
+	}
+	
+	public void setAlpha(int value){
+		alpha = value;
 	}
 
 }
--- src/com/rajankz/personalmessage/MainActivity.java
+++ src/com/rajankz/personalmessage/MainActivity.java
@@ -1,18 +1,121 @@
 package com.rajankz.personalmessage;
 
+import java.io.ByteArrayOutputStream;
+import java.io.FileOutputStream;
+
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
+	SeekBar seekRed, seekGreen, seekBlue, seekAlpha;
+	EditText valRed, valGreen, valBlue, valAlpha;
+	DrawingView drawingCanvas;
+	Button clearCanvas, shareCanvas;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		initContentView();
 	}
+	
+	public void initContentView(){
+		seekRed = (SeekBar) findViewById(R.id.seekRed);
+		seekGreen = (SeekBar) findViewById(R.id.seekGreen);
+		seekBlue = (SeekBar) findViewById(R.id.seekBlue);
+		seekAlpha = (SeekBar) findViewById(R.id.seekAlpha);
+		
+		valRed = (EditText) findViewById(R.id.valRed);
+		valGreen = (EditText) findViewById(R.id.valGreen);
+		valBlue = (EditText) findViewById(R.id.valBlue);
+		valAlpha = (EditText) findViewById(R.id.valAlpha);
+		
+		seekRed.setOnSeekBarChangeListener(seekChangeListener);
+		seekGreen.setOnSeekBarChangeListener(seekChangeListener);
+		seekBlue.setOnSeekBarChangeListener(seekChangeListener);
+		seekAlpha.setOnSeekBarChangeListener(seekChangeListener);
+		
+		drawingCanvas = (DrawingView)findViewById(R.id.drawingCanvas);
+		drawingCanvas.setOnTouchListener(drawingCanvas);
+		
+		clearCanvas = (Button)findViewById(R.id.clearCanvas);
+		clearCanvas.setOnClickListener(clearCanvasClick);
+		
+		shareCanvas = (Button)findViewById(R.id.shareCanvas);
+		shareCanvas.setOnClickListener(sendMessageClick);
+	}
+	
+	private SeekBar.OnSeekBarChangeListener seekChangeListener = new SeekBar.OnSeekBarChangeListener() {
+		@Override
+		public void onStopTrackingTouch(SeekBar seekBar) {
+		}
+		
+		@Override
+		public void onStartTrackingTouch(SeekBar seekBar) {
+		}
+		
+		@Override
+		public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+			switch(seekBar.getId()){
+				case R.id.seekRed:
+					valRed.setText(""+progress);
+					drawingCanvas.setRed(progress);
+					break;
+				case R.id.seekGreen:
+					valGreen.setText(""+progress);
+					drawingCanvas.setGreen(progress);
+					break;
+				case R.id.seekBlue:
+					valBlue.setText(""+progress);
+					drawingCanvas.setBlue(progress);
+					break;
+				case R.id.seekAlpha:
+					valAlpha.setText(""+progress);
+					drawingCanvas.setAlpha(progress);
+					break;
+			}
+		}
+	};
 
+	private OnClickListener clearCanvasClick = new OnClickListener() {
+		@Override
+		public void onClick(View v) {
+			if(drawingCanvas != null){
+				drawingCanvas.clearCanvas();
+			}
+		}
+	};
+	
+	private OnClickListener sendMessageClick = new OnClickListener() {
+		@Override
+		public void onClick(View v) {
+			Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
+			Bitmap canvas = drawingCanvas.getDrawingCache(); //need to enable drawing cache in DrawingView
+			if(canvas != null){
+				//TODO: generate sharing image data here
+				//TODO: set the image data in intent extra
+				canvas.recycle();
+				sharingIntent.setType("image/png");
+				startActivity(Intent.createChooser(sharingIntent, "Share via"));
+			}else{
+				Toast.makeText(getBaseContext(), R.string.err_no_canvas_image, Toast.LENGTH_SHORT).show();
+			}
+		}
+	};
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
